aaf2a7678595859f317f52537bf8b0ae8288e33d
313581041DanielHsu <danielhsu.ii13@nycu.edu.tw>
2025-03-03 17:58:58 +0800

    Update problem45.cpp

--- week02/problem45/problem45.cpp
+++ week02/problem45/problem45.cpp
@@ -1,1 +1,31 @@
-
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int times=0;
+        if(nums.size()==1){
+            return times;
+        }
+        for(int i=0;i<nums.size();i++){
+            int best=0,bestpos=0;
+            times++;
+            for(int j=1;j<=nums[i]&&(i+j)<nums.size();j++){
+                if(j+nums[i+j]>=best){
+                    best=j+nums[i+j];
+                    bestpos=i+j;
+                }
+                if((i+j)==nums.size()-1){
+                    return times;
+                }
+            }
+            cout<<"i= "<<i<<endl;
+            cout<<"best= "<<best<<endl;
+            cout<<"bestpos= "<<bestpos<<endl;
+            cout<<"times= "<<times<<endl;
+            i=bestpos-1;
+            if(bestpos>=nums.size()-1){
+                break;
+            }
+        }
+        return times;
+    }
+};
