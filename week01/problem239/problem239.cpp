24c17263762c497553ebeb6ad9a80727a29ee87d
313581041DanielHsu <danielhsu.ii13@nycu.edu.tw>
2025-03-03 20:50:41 +0800

    Update problem239.cpp

--- week01/problem239/problem239.cpp
+++ week01/problem239/problem239.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> result;
+        deque<int> dq;
 
+        for (int i = 0; i < nums.size(); i++) {
+            if (!dq.empty() && dq.front() == i - k) {
+                dq.pop_front();
+            }
+
+            while (!dq.empty() && nums[dq.back()] <= nums[i]) {
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+
+            if (i >= k - 1) {
+                result.push_back(nums[dq.front()]);
+            }
+        }
+
+        return result;
+    }
+};
+/*我們可以使用 deque（雙端隊列） 來維護 單調遞減窗口，確保每次都能 O(1) 取得當前窗口最大值。*/
