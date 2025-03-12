54a6e7e99f15ccda8e7b3bbd8e5826605945eda6
DanielHsu <danielhsu.ii13@nycu.edu.tw>
2025-03-12 23:32:04 +0800

    Update problem56.cpp

--- extra_practice/problem56/problem56.cpp
+++ extra_practice/problem56/problem56.cpp
@@ -1,1 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[0] < b[0]; 
+        });
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i][0]<=intervals[i-1][1]){
+                if(intervals[i-1][1]>intervals[i][1]){
+                    intervals[i][1]=intervals[i-1][1];
+                    intervals[i][0]=intervals[i-1][0];
+                    intervals[i-1][0]=-1;
+                    intervals[i-1][1]=-1;
+                }else{
+                    intervals[i][0]=intervals[i-1][0];
+                    intervals[i-1][0]=-1;
+                    intervals[i-1][1]=-1;
+                }
+            }
+        }
+        for(int i=0;i<intervals.size();i++){
+            if(intervals[i][0]!=-1&&intervals[i][1]!=-1){
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
+    }
+};
+
+
 
