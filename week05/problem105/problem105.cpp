81d2f84dd869046e0cc5166fe182d3483299ad5a
DanielHsu <danielhsu.ii13@nycu.edu.tw>
2025-03-25 09:37:25 +0800

    Update problem105.cpp

--- week05/problem105/problem105.cpp
+++ week05/problem105/problem105.cpp
@@ -1,1 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    unordered_map<int, int> inorderMap;
+    int preorderIndex = 0;
 
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        for (int i = 0; i < inorder.size(); i++) {
+            inorderMap[inorder[i]] = i;
+        }
+        return build(preorder, 0, inorder.size() - 1);
+    }
+
+private:
+    TreeNode* build(vector<int>& preorder, int start, int end) {
+        if (start > end) return nullptr;
+
+        int rootVal = preorder[preorderIndex++];
+        TreeNode* root = new TreeNode(rootVal);
+
+        int inorderIndex = inorderMap[rootVal];
+
+        root->left = build(preorder, start, inorderIndex - 1);
+        root->right = build(preorder, inorderIndex + 1, end);
+
+        return root;
+    }
+};
