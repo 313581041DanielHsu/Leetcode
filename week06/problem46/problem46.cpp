b89bc5b138fe0ca0a27efbbf12c7880badb00c16
DanielHsu <danielhsu.ii13@nycu.edu.tw>
2025-04-02 14:12:20 +0800

    Update problem46.cpp

--- week06/problem46/problem46.cpp
+++ week06/problem46/problem46.cpp
@@ -1,1 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> path;
+        vector<bool> used(nums.size(), false);
+        backtrack(nums, used, path, result);
+        return result;
+    }
+
+    void backtrack(vector<int>& nums, vector<bool>& used, 
+                   vector<int>& path, vector<vector<int>>& result) {
+        if (path.size() == nums.size()) {
+            result.push_back(path);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (used[i]) continue;
+
+            // 做選擇
+            path.push_back(nums[i]);
+            used[i] = true;
+
+            // 繼續遞迴
+            backtrack(nums, used, path, result);
+
+            // 回溯：撤銷選擇
+            path.pop_back();
+            used[i] = false;
+        }
+    }
+};
 
