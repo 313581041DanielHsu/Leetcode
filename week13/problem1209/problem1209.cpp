f44d33aa081831d120197fe1eea2a1fb8531e1f8
DanielHsu <danielhsu.ii13@nycu.edu.tw>
2025-05-20 16:07:58 +0800

    Update problem1209.cpp

--- week13/problem1209/problem1209.cpp
+++ week13/problem1209/problem1209.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        stack<pair<char, int>> stk;
+        for (char c : s) {
+            if (!stk.empty() && stk.top().first == c) {
+                stk.top().second++;
+                if (stk.top().second == k)
+                    stk.pop();
+            } else {
+                stk.push({c, 1});
+            }
+        }
+        
+        string result;
+        while (!stk.empty()) {
+            result = string(stk.top().second, stk.top().first) + result;
+            stk.pop();
+        }
+        return result;
+    }
+};
